2e70bbe617db10957ff368fd7353001542faa0c3
jr_ts_31 <104570136+kamblerutik@users.noreply.github.com>
2025-04-17 22:24:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ import connect from "./src/lib/db.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors())
 app.use("/api", router)
 
